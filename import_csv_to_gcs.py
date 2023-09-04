09e540c46fca3e096ec1a470179e81af786e72c3
phakawatfong <phakawatfong@gmail.com>
2023-09-04 17:04:04 +0700

    add script to upload data from local into google cloud storagegit add import_csv_to_gcs.py poetry.lock  pyproject.toml !

---
+++ import_csv_to_gcs.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+from google.cloud import storage
+from google.oauth2 import service_account
+
+# Setup configuration path AND target path.
+root_path = os.getcwd()
+config_path = f"{root_path}\env_conf"
+keyfile = f"{config_path}\greenery-398007-e5bfbcaa9438.json"
+DATA_FOLDER = f"{root_path}\data"
+BUSINESS_DOMAIN = "greenery"
+
+# Setup credentails for GoogleCloudStorage Connection
+# ref: https://google-auth.readthedocs.io/en/master/reference/google.oauth2.service_account.html
+service_account_info = json.load(open(keyfile))
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
+
+# ref: https://gcloud.readthedocs.io/en/latest/storage-buckets.html
+def import_csv_to_gcs(file_name):
+    table_name= file_name.split(".csv")[0]
+    project_id = "greenery-398007"
+    bucket_name = "greenery-bucket"
+    source_file_path = f"{DATA_FOLDER}\{table_name}.csv"
+    destination_blob_name = f"{BUSINESS_DOMAIN}/{table_name}/{table_name}.csv"
+
+    # Connect to GoogleCloudStorage
+    # ref: https://gcloud.readthedocs.io/en/latest/storage-client.html
+    storage_client = storage.Client(project=project_id, credentials=credentials)
+
+    # ref: https://gcloud.readthedocs.io/en/latest/storage-blobs.html
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_path)
+
+    print(f"File {table_name} uploaded to GoogleCloudStorage: {destination_blob_name}.")
+
+if __name__ == "__main__":
+    print(os.listdir(DATA_FOLDER))
+    greenery_file_list = list(os.listdir(DATA_FOLDER))
+    
+    for table_name in greenery_file_list:
+        import_csv_to_gcs(table_name)
+

d97cdbb70943d3c560fc2ff4f39fe0bca46982a5
phakawatfong <phakawatfong@gmail.com>
2023-09-04 13:27:39 +0700

    add script to get data from API

---
+++ main-api.py
@@ -0,0 +1,37 @@
+import csv
+import configparser
+
+import requests
+
+# create configObject to read configuration from pipeline.conf file
+config = configparser.ConfigParser()
+config.read("pipeline.conf")
+host = config.get("api_config", "host")
+port = config.get("api_config", "port")
+
+API_URL = f"http://{host}:{port}"
+
+# print(API_URL)
+
+DATA_FOLDER = "data"
+table_list = ["addresses", "events", "order-items", "orders", "products", "promos", "users"]
+# table_list = ["addresses", "events"]
+
+
+
+for tbl in table_list:
+    # print(f"{API_URL}/{tbl}")
+    response = requests.get(f"{API_URL}/{tbl}")
+    data = response.json()
+
+    with open(f"{DATA_FOLDER}/{tbl}.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        header = data[0].keys()
+        # print(writer)
+        # print(header)
+        writer.writerow(header)
+
+        for each in data:
+            writer.writerow(each.values())
+
+

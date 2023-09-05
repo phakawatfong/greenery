c8213a08a2203166fc064d0c9c193f084d5fcfd3
phakawatfong <phakawatfong@gmail.com>
2023-09-05 21:41:34 +0700

    refactor delete unecessary parameter ,and encapsulated the old-code with function to perform automate-load every table(s) from GCS to BigQuery

--- gcs_to_bigquery_load.py
+++ gcs_to_bigquery_load.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import os
 
@@ -14,12 +13,7 @@ DATA_FOLDER = f"{root_path}\data"
 project_id = "greenery-398007"
 bucket_name = "greenery-bucket"
 location = "asia-southeast1"
-
 BUSINESS_DOMAIN = "greenery"
-table_name = "addresses"
-source_file_path = f"{DATA_FOLDER}\{table_name}.csv"
-destination_blob_name = f"{BUSINESS_DOMAIN}/{table_name}/{table_name}.csv"
-
 
 # configuration for GCS
 keyfile_gcs = f"{config_path}\greenery-google-cloud-storage.json"
@@ -37,25 +31,37 @@ bigquery_client = bigquery.Client(credentials = credentials_bq, location = locat
 
 # load data from gcs to bigquery ref : https://cloud.google.com/python/docs/reference/bigquery/latest/google.cloud.bigquery.client.Client#google_cloud_bigquery_client_Client_load_table_from_uri
 # https://cloud.google.com/python/docs/reference/bigquery/latest/google.cloud.bigquery.job.WriteDisposition
-table_id = f"{project_id}.{BUSINESS_DOMAIN}.{table_name}"
+def load_data_from_gcs_to_bigquery(file_name):
 
-# gs://greenery-bucket/greenery/addresses/addresses.csv
-uri = f"gs://{bucket_name}/{destination_blob_name}"
-job_config = bigquery.LoadJobConfig(
-                skip_leading_rows = 1,
-                write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE,
-                source_format = bigquery.SourceFormat.CSV,
-                autodetect = True,
-                )
+    table_name= file_name.split(".csv")[0]
+    destination_blob_name = f"{BUSINESS_DOMAIN}/{table_name}/{table_name}.csv"
+    table_id = f"{project_id}.{BUSINESS_DOMAIN}.{table_name}"
 
-load_job = bigquery_client.load_table_from_uri(
-                                    uri,
-                                    table_id,
-                                    job_config = job_config,
-                                    location = location,
-                                    )
+    # gs://greenery-bucket/greenery/addresses/addresses.csv
+    uri = f"gs://{bucket_name}/{destination_blob_name}"
+    job_config = bigquery.LoadJobConfig(
+                    skip_leading_rows = 1,
+                    write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE,
+                    source_format = bigquery.SourceFormat.CSV,
+                    autodetect = True,
+                    )
 
-load_job.result()  # Waits for the job to complete.
+    load_job = bigquery_client.load_table_from_uri(
+                                        uri,
+                                        table_id,
+                                        job_config = job_config,
+                                        location = location,
+                                        )
 
-destination_table = bigquery_client.get_table(table_id)  # Make an API request.
-print(f"GoogleCloudStorage ---> Bigquery: Loaded {destination_table.num_rows} rows and {len(destination_table.schema)} columns to {table_id}")
+    load_job.result()  # Waits for the job to complete.
+
+    destination_table = bigquery_client.get_table(table_id)  # Make an API request.
+    print(f"GoogleCloudStorage ---> Bigquery: Loaded {destination_table.num_rows} rows and {len(destination_table.schema)} columns to {table_id}")
+
+
+if __name__ == "__main__":
+    print(os.listdir(DATA_FOLDER))
+    greenery_file_list = list(os.listdir(DATA_FOLDER))
+    
+    for table_name in greenery_file_list:
+        load_data_from_gcs_to_bigquery(table_name)
